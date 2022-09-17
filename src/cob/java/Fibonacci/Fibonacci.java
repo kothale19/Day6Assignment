3d624a6db09721963b3529d65b03945dfddb8e90
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-17 17:16:42 +0530

    Added: FibonacciSeries

--- src/cob/java/Fibonacci/Fibonacci.java
+++ src/cob/java/Fibonacci/Fibonacci.java
@@ -2,9 +2,36 @@ package cob.java.Fibonacci;
 
 public class Fibonacci {
 
-	public static void main(String[] args) {
-	System.out.println("Welcome to Fibonacci Series Program!!");
+	
+	 static void getFibonacci(int range)
+	   {
+	           
+	              int f1=0;
+	              int f2=1;
+	              System.out.print(f1 +" "+f2+" " );
+	          
+	          for(int i=1;i<=range;i++)
+	         {
+	            int f3 = f1 + f2;
+	          System.out.print(f3+ " ");
+	           f1 = f2;
+	           f2 = f3;
+	           
+	         }
 
-	}
+	   }
 
-}
+	
+	          public static void main(String[] args) {
+	       System.out.println("Welcome to Fibonacci Series Program!!");
+	        	    getFibonacci(10); 
+	           
+	          }
+                        } 
+	
+	
+	
+	
+	
+
+
