ec1b5524a73ae393ddc429009ff5c066da44b514
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-17 18:19:13 +0530

    Added:Perfect Number Computation Logic

--- src/cob/java/Fibonacci/PerfectNumber.java
+++ src/cob/java/Fibonacci/PerfectNumber.java
@@ -1,10 +1,24 @@
 package cob.java.Fibonacci;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to Perfect Number Program!!");
+		System.out.println("Welcome to Perfect Number Program!!");
+		System.out.println("Enter any Number:");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				sum = sum + i;
+			}
+		}
+		if (n == sum)
+			System.out.println("it is a Perfect Number");
+		else
+			System.out.println("It is not a Perfect Number");
 
 	}
-
 }
